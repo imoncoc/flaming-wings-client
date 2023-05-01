c9bb5bf023728ff25dd86cd525d6b4e4630e881c
Md Imon Hossain <imoncoc0@gmail.com>
2023-05-01 23:11:00 +0600

    Navbar, custom color, home image added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import './Home.css'
+import flamingWings from '../../assets/images/flaming-wings.jpg'
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is HOme</h2>
+      <div className="wings-home-bg">
+        <div className="container-fluid ">
+          <div className="row">
+            <div className="col-12 mx-auto text-white">
+              <h2 className=''>This is Food</h2>
+            </div>
+          </div>
         </div>
+      </div>
     );
 };
 
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,10 +1,82 @@
 import React from 'react';
+import './Header.css'
+import { NavLink } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import wings_logo from '../../assets/images/flaming-wings-logo.jpg'
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is Header</h2>
-        </div>
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-light px-5 py-3">
+          <Link to="/" className="navbar-brand">
+            {/* <h3>Book Now</h3> */}
+            <img src={wings_logo} alt="" className='img-fluid wings-logo'  />
+          </Link>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#myNavbar"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="myNavbar">
+            <ul className="navbar-nav mx-auto">
+              <li className="nav-item">
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link default"
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+              {/* {!user && (
+                <li className="nav-item">
+                  <NavLink
+                    to="/login"
+                    className={({ isActive }) =>
+                      isActive ? "nav-link active" : "nav-link default"
+                    }
+                  >
+                    Login
+                  </NavLink>
+                </li>
+              )} */}
+              <NavLink
+                to="/login"
+                className={({ isActive }) =>
+                  isActive ? "nav-link active" : "nav-link default"
+                }
+              >
+                Login
+              </NavLink>
+
+              <li className="nav-item">
+                <NavLink
+                  to="/confirmBooking"
+                  className={({ isActive }) =>
+                    isActive ? "nav-link active" : "nav-link default"
+                  }
+                >
+                  Confirm Booking
+                </NavLink>
+              </li>
+            </ul>
+            <div className="d-flex d-lg-flex align-items-baseline justify-content-between">
+              <p className="text-white me-4">
+                {/* <span className="me-2 phone-icon">
+                  <FontAwesomeIcon icon={faUser} />
+                </span>
+                {user ? user?.displayName : "No User"} */}
+                Hello User
+              </p>
+              <button className="btn btn-danger">Logout</button>
+            </div>
+          </div>
+        </nav>
+      </div>
     );
 };
 
