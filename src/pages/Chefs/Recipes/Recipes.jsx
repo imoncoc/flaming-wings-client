3117d58448814a238f8705add1064a9c260a024b
Md Imon Hossain <imoncoc0@gmail.com>
2023-05-02 23:57:56 +0600

    recipes card added, react-rating added

--- src/pages/Chefs/Recipes/Recipes.jsx
+++ src/pages/Chefs/Recipes/Recipes.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { set } from 'react-hook-form';
 import { Link } from 'react-router-dom';
+import './Recipes.css'
 
 const Recipes = ({recipe}) => {
     console.log(recipe)
-    const { instructions, ratings } = recipe;
+    const {
+      instructions,
+      ratings,
+      strMeal,
+      strIngredient1,
+      strIngredient2,
+      strIngredient3,
+      strIngredient4,
+      strIngredient5
+    } = recipe;
+    const [fold, setFold] = useState(true);
+    // console.log(instructions);
     return (
       <div className="col-10 col-md-6 col-lg-4 mx-auto my-5">
         <div
@@ -19,10 +32,30 @@ const Recipes = ({recipe}) => {
                 year: "numeric",
               })}
             </p>
-            <h5 className="card-title">{recipe.strMeal}</h5>
-            <p className="card-text">
-              There are many variations of passages of Lorem Ipsum available.
-            </p>
+            <h5 className="card-title">{strMeal}</h5>
+            {fold ? (
+              <>
+                <span className="card-title">
+                  {instructions.substring(0, 100)}....
+                </span>
+                <span className=" read-more-btn" onClick={() => setFold(!fold)}>
+                  Read More
+                </span>
+              </>
+            ) : (
+              <>
+                <span className="card-title">{instructions} &nbsp; &nbsp;</span>
+                <span className="read-more-btn" onClick={() => setFold(!fold)}>
+                  Read Less
+                </span>
+              </>
+            )}
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                Ingredient: {strIngredient1}, {strIngredient2}, {strIngredient3}
+                , {strIngredient4}, {strIngredient5}
+              </li>
+            </ul>
             <Link href="#" className="nav-link tea-color-primary fw-semibold">
               Learn More
             </Link>
